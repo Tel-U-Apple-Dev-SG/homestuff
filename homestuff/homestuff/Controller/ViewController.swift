9607034bcc59e18b8c6b7b14a3342f56e6a02cc5
Kevin M Althaf <kevineksklusif@gmail.com>
2025-01-17 13:32:17 +0700

    menambah dasar layout home page

--- homestuff/homestuff/Controller/ViewController.swift
+++ homestuff/homestuff/Controller/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    
+    @IBOutlet weak var VIewTotalBarang: UILabel!
+    
+    @IBOutlet weak var ViewBarangKadaluarsaTerdekat: UILabel!
+    
+    override func viewDidLoad()
+    {            super.viewDidLoad()
+        
     }
+    
 
-
+    @IBAction func ProfilePIcturePressed(_ sender: UIButton) {
+        print("Profile picture Pressed")
+    }
+    
+    
+    @IBAction func CheckSelengkapnyaPressed(_ sender: UIButton) {
+        print("Check selengkapnya tapped")
+    }
+    
+    @IBAction func AddItemPressed(_ sender: UIButton) {
+        print("Tambah barang tapped")
+    }
+    
+    @IBAction func InfoAppPressed(_ sender: UIButton) {
+        print("Info App tapped")
+    }
+    
+    @IBAction func SearchItemPressed(_ sender: Any) {
+        print("Search tapped")
+    }
+    
+    @IBAction func NotificationPressed(_ sender: UIButton) {
+        print("Notifikasi tapped")
+    }
+    
+    @IBAction func HistoryItemAddedPressed(_ sender: Any) {
+        print("Histori barang tapped")
+    }
 }
 
