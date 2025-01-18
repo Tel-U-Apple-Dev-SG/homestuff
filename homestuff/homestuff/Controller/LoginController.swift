77b3f3683f4e43cb0db599cf3bc0d7ee3611ff3b
Muhammad Daffa Izzati <m.daffaizzati@gmail.com>
2025-01-18 16:59:59 +0700

    Menambah controller untuk halaman register dan menambahkan segue dari halaman login ke register

--- homestuff/homestuff/Controller/LoginController.swift
+++ homestuff/homestuff/Controller/LoginController.swift
@@ -8,8 +8,31 @@
 import UIKit
 
 class LoginController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+    }
+    
+    @IBAction func registerLinkPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "moveToRegister", sender: sender)
+        print("Move to register")
+    }
+    
+    @IBAction func signInButtonPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "moveToHome", sender: sender)
+        print("Signed in")
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "moveToRegister" {
+                if let destinationVC = segue.destination as? RegisterController {
+                    // Kirim data ke RegisterViewController jika diperlukan
+                    // destinationVC.someProperty = someValue
+                }
+            }
     }
 }
---
+++ homestuff/homestuff/Controller/RegisterController.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterController.swift
+//  homestuff
+//
+//  Created by Muhammad Daffa Izzati on 18/01/25.
+//
+
+import UIKit
+
+class RegisterController: UIViewController {
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+}
+
