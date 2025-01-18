5baa1dc8a5d8a482d7eaef30e5907f8f160e4960
Muhammad Daffa Izzati <m.daffaizzati@gmail.com>
2025-01-18 12:30:53 +0700

    Menambahkan halaman register dan memperbaiki storyboard

---
+++ homestuff/homestuff/Controller/LoginController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginController.swift
+//  homestuff
+//
+//  Created by Muhammad Daffa Izzati on 18/01/25.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
